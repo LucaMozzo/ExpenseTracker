c8c28f6fb87292f722e63d305d76e3f2ad6808a5
lucamozzo <lucamozzo21173@gmail.com>
2016-12-16 17:30:32 +0000

    Added execute query method

--- src/main/java/model/Database.java
+++ src/main/java/model/Database.java
@@ -2,8 +2,8 @@ package model;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
-
 
 /**
  * @author Luca Mozzo
@@ -14,6 +14,7 @@ public final class Database {
     /**
      * Executes a command
      * @param command the command
+     * @return the number of rows affected
      */
     public static int executeNonQuery(String command){
         Connection connection = null;
@@ -30,6 +31,29 @@ public final class Database {
             return result;
         } catch(Exception ex){
             return 0;
+        }
+    }
+
+    /**
+     * Executes a query
+     * @param query the query
+     * @return the result
+     */
+    public static ResultSet executeQuery(String query){
+        Connection connection = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:data.db");
+            connection.setAutoCommit(true);
+
+            Statement stmt = connection.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+
+            stmt.close();
+            connection.close();
+            return rs;
+        } catch(Exception ex){
+            return null;
         }
     }
 }
