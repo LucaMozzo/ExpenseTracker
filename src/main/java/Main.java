8d263310361fb1d74d410e808bdcee2a59bbad2c
lucamozzo <lucamozzo21173@gmail.com>
2016-12-16 11:50:27 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,16 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * @author Luca Mozzo
+ * Main class of the application
+ */
+public class Main extends Application{
+    public static void main(String[] args){
+        launch(args);
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+}
---
+++ src/main/java/view/Login.java
@@ -0,0 +1,8 @@
+package view;
+
+/**
+ * @author Luca Mozzo
+ * Login diaog for the application
+ */
+public class Login {
+}
